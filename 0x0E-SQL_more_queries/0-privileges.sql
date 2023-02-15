1bcc7e46b4e8ae9a60a3348af75f97f2f04da0c7
natiteg <natitegg@gmail.com>
2023-02-15 12:15:51 -0800

    listing priveleges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
-SHOW GRANTS FOR user_0d_1@localhost; -- list all priveleges of user_0d_1
-SHOW GRANTS FOR user_0d_2@localhost; -- list all priveleges of user_0d_2
+ -- list all priveleges of user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost; 
+ -- list all priveleges of user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost; 
 
 
