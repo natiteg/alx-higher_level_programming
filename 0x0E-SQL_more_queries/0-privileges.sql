3c6fe607ea72b5e90059700e02eca8af665e4d35
natiteg <natitegg@gmail.com>
2023-02-15 12:12:55 -0800

    listing priveleges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
-CREATE USER 'user_0d_1'@'localhost';  -- creates user_0d_1 on localhost
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; -- grants privileges to user_0d_1
-CREATE USER 'user 0d_2'@'localhost';  -- creates user_0d_2 on localhost
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost'; -- grants privileges to user_0d_2
+SHOW GRANTS FOR user_0d_1@localhost; -- list all priveleges of user_0d_1
+SHOW GRANTS FOR user_0d_2@localhost; -- list all priveleges of user_0d_2
+
 
