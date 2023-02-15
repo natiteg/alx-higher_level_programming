1b9e42e44e670f8c8c43d50de1bfba720dc35657
natiteg <natitegg@gmail.com>
2023-02-15 12:06:17 -0800

    creating users and granting priveleges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+CREATE USER 'user_0d_1'@'localhost';  -- creates user_0d_1 on localhost
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; -- grants privileges to user_0d_1
+CREATE USER 'user 0d_2'@'localhost';  -- creates user_0d_2 on localhost
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost'; -- grants privileges to user_0d_2
+
