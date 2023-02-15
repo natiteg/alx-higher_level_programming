752ebfca9915159d6c48f77566edce0ca04fedaa
natiteg <natitegg@gmail.com>
2023-02-15 12:17:11 -0800

    listing priveleges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
- -- list all priveleges of user_0d_1
+ -- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+
 SHOW GRANTS FOR user_0d_1@localhost; 
- -- list all priveleges of user_0d_2
 SHOW GRANTS FOR user_0d_2@localhost; 
 
 
