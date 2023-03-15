8c251ddcb61e8737c74dc3fd9b90e85720146927
natiteg <natitegg@gmail.com>
2023-03-15 13:43:25 -0700

    added files

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const concat = 'C is fun\n' + 'Python is cool\n' + 'Javascript is amaizing\n';
+const concat = 'C is fun\n' + 'Python is cool\n' + 'Javascript is amaizing';
 console.log(concat);
